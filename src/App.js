4eb3c308546de91993eeddfad1e85155826e04ce
Sahana <sahana.harish@students.mq.edu.au>
2021-10-05 18:10:24 +1100

    Update App.js
    Added static placeholder text  for Activity Leaderboard section (Sprint Report 1)

--- src/App.js
+++ src/App.js
@@ -138,6 +138,11 @@ const App = () => {
     
         <Route path="/leaderboard">
           <h1>Activity Leaderboard</h1>
+          <p>[1] User 1 - FIRST PLACE</p>
+          <p>[2] User 4 - SECOND PLACE</p>
+          <p>[3] User 2 - THIRD PLACE</p>
+          <p>[4] User 5 - FOURTH PLACE</p>
+          <p>[5] User 4 - FIFTH PLACE</p>
         </Route>
 
         <Route path="/my">

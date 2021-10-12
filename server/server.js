3240e988211572d841b91fa7a633c84388e51915
SL-MQ <53845087+SL-MQ@users.noreply.github.com>
2021-10-12 22:24:51 +1100

    deleted sample activities

--- server/server.js
+++ server/server.js
@@ -6,64 +6,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-//sample activities
-let activities = [
-    {
-        "activity": "Learn Express.js",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test1",
-        "id": 0
-    },
-    {
-        "activity": "Bake something you've never tried before",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test1",
-        "id": 1
-    },
-    {
-        "activity": "Learn how to play a new sport",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test1",
-        "id": 2
-    },
-    {
-        "activity": "Text a friend you haven't talked to in a long time",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test1",
-        "id": 3
-    },
-    {
-        "activity": "Meditate for five minutes",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test1",
-        "id": 4
-    },
-    {
-        "activity": "Learn to play a new instrument",
-        "accessibility": 0.1,
-        "type": "social",
-        "participants": 1,
-        "price": 0.0,
-        "username": "test2",
-        "id": 5
-    }
-]
-
 let users = [
     {
         "username": "test1",

1ac6a8e45ed36866380da10c3a06a57dd64e9332
joshliew <53844836+joshliew@users.noreply.github.com>
2021-10-30 23:47:36 +1100

    Uninstalled heroku-ssl-redirect (was causing some npm errors) and inserted a <meta> tag in index.html to try fix the Mixed Content error in Chrome

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-import sslRedirect from 'heroku-ssl-redirect'
 const express = require('express') 
 const cors = require("cors")
 const apiRouter = require("./controllers/api")
@@ -8,6 +7,5 @@ app.use(cors())
 app.use(express.json())
 app.use(apiRouter)
 app.use(express.static("build"))
-app.use(sslRedirect())
 
 module.exports = app

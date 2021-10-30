cbbb31792058f382e02515916d906f1db0afd2c8
joshliew <53844836+joshliew@users.noreply.github.com>
2021-10-30 21:59:57 +1100

    Moved reference to build directory from server.js to app.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
-const express = require('express')
 require('dotenv').config()
 const app = require('./app')
-app.use(express.static("build"))
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {

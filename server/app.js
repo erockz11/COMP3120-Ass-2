8499186a6ce1d7c7312b37198b254797f5ba5bd0
joshliew <53844836+joshliew@users.noreply.github.com>
2021-10-30 23:37:24 +1100

    Installed heroku-ssl-redirect

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import sslRedirect from 'heroku-ssl-redirect'
 const express = require('express') 
 const cors = require("cors")
 const apiRouter = require("./controllers/api")
@@ -7,5 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(apiRouter)
 app.use(express.static("build"))
+app.use(sslRedirect())
 
 module.exports = app
